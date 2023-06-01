32d214010daccc11928ab109798fd032940ca497
R1no933 <dbaskakov93@yandex.ru>
2023-06-01 10:11:58 +0300

    add test for horse class getName func(complete task 1b)

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HorseTest {
@@ -37,5 +39,15 @@ public class HorseTest {
     public void distanceNotNegativeException() {
         IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> new Horse("some name", 1, -1));
         assertEquals("Distance cannot be negative.", error.getMessage());
+    }
+
+    @Test
+    public void getNameTest() throws NoSuchFieldException, IllegalAccessException {
+        Horse horse = new Horse("qwerty", 1, 1);
+
+        Field horseName = Horse.class.getDeclaredField("name");
+        horseName.setAccessible(true);
+        String fieldValue = (String) horseName.get(horse);
+        assertEquals("qwerty", fieldValue);
     }
 }
